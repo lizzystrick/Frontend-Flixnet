70ce7bcad58879116bfd975c50ec2ca840e77aab
Lizzy <447704@student.fontys.nl>
2023-10-17 15:17:07 +0200

    Update app.module.ts

--- FlixnetFrontend/src/app/app.module.ts
+++ FlixnetFrontend/src/app/app.module.ts
@@ -5,18 +5,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SearchComponent } from './search/search.component';
-import { CardMovieComponent } from './card-movie/card-movie.component';
-import { CardSerieComponent } from './card-serie/card-serie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    SearchComponent,
-    CardMovieComponent,
-    CardSerieComponent
+
   ],
   imports: [
     BrowserModule,
