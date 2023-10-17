66434ffbc92bb377892f373ffb54c8bf89678185
Lizzy <447704@student.fontys.nl>
2023-10-17 15:15:52 +0200

    wijzigingen

--- FlixnetFrontend/src/app/app.module.ts
+++ FlixnetFrontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { CardMovieComponent } from './card-movie/card-movie.component';
+import { CardSerieComponent } from './card-serie/card-serie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    SearchComponent,
+    CardMovieComponent,
+    CardSerieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FlixnetFrontend/src/app/home/home.component.ts
+++ FlixnetFrontend/src/app/home/home.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Cards {
+  keepWatching?: Array<number>;
+  popular?: Array<number>;
+}
+
+interface Series {
+  cardImage?: String;
+  titleImage?: String;
+  backgroundImage?: String;
+  relevance?: number;
+  year?: number;
+  minAge?: number;
+  time?: number;
+  season?: null;
+  description?: String;
+  cast?: Array<String>;
+  genre?: Array<String>;
+  scenes?: Array<String>;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +26,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor() {}
+  show = false;
+  serie: Series = {};
+  userId = '1';
+  loading = false;
+  cards: Cards = {};
+  popular?: Array<number> = [];
+  popular2 = ['5', '6', '7'];
+  keepWatching?: Array<number> = [];
+  url: string = 'https://private-3923c4-santandercoders809.apiary-mock.com/';
+
+
+  close() {
+    this.serie = {};
+    this.show = false;
+  }
 }
